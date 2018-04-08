23031db223e90ab5855163f2b9357e6195abb404
rj <rajibbajracharya@gmail.com>
2018-04-08 10:11:19 +0545

    layout for StudentProfile.vue

--- src/views/StudentProfile.vue
+++ src/views/StudentProfile.vue
@@ -9,46 +9,46 @@ export default {
     ifSmallSized () { return this.$vuetify.breakpoint.smAndDown },
     masterLayout () {
       const binding = {}
-      if (this.$vuetify.breakpoint.xsAndDown) {
-        binding.column = true
-        binding.wrap = true
-      }
-      if (this.$vuetify.breakpoint.mdAndUp) {
-        binding.row = true
-        binding.column = false
-      }
-      return binding
-    },
-    masterContainer () {
-      const binding = {}
       if (this.$vuetify.breakpoint.smAndDown) {
-        binding.fluid = true
+        console.log('smAndDown')
+      } else if (this.$vuetify.breakpoint.mdAndUp) {
+        console.log('mdAndUp')
+      } else if (this.$vuetify.breakpoint.xsAndDown) {
+        console.log('xsAndDown')
       }
-      return binding
+
+      // if (this.$vuetify.breakpoint.xsAndDown) {
+      //   binding.column = true
+      //   binding.row = false
+      // }
+      // if (this.$vuetify.breakpoint.mdAndUp) {
+      //   binding.row = true
+      // }
+      // return binding
     }
   }
 }
 </script>
 
 <template lang="pug">
-v-container(container)
-  v-layout(row wrap justify-center)
+v-container(container grid-list-md)
+  v-layout(justify-center).sm-col
     
     // profile bill board
-    v-flex(d-flex xs12 sm8 md3)
-      v-card(color="indigo" dark)
+    v-flex(d-flex).profile-board.sm-full
+      v-card.profile-board-pad
         v-layout(column)
           v-layout(justify-center column).sm-row
             // avatar container
             v-flex(d-flex xs5 sm5 md12)
-              div
-                v-avatar(:tile="false" :size="156")
+              v-flex(d-flex justify-center pb-2)
+                v-avatar(:tile="false" :size=156)
                   img(src="../assets/svg/avatar.svg")
             // name container
             v-flex(d-flex xs7 sm7 md12)
               v-layout(column)
-                h2 Suru Dururu
-                p This is a shorthand flex-direction and flex-wrap properties, which together define the flex container's main and cross axes.
+                h2.text-sm-center Suru Dururu
+                p.text-sm-justify This is a shorthand flex-direction and flex-wrap properties, which together define the flex container's main and cross axes.
           v-layout(justify-center column).sm-row
             v-flex
               div Email
@@ -69,49 +69,83 @@ v-container(container)
               img(src="../assets/svg/twitter.svg" height="36") 
 
     // info cards
-    v-flex(d-flex xs12 sm12 md8)
-      v-layout(wrap align-content-start)
-        v-flex(d-flex xs12 sm6)
+    v-flex(d-flex).info-card-container.sm-full
+      v-layout(wrap align-content-start).info-card-layout
+        v-flex(d-flex).info-card
           v-layout(column)
             v-flex
-              v-card(color="red" dark)
+              v-card
                 v-card-title Profile Bill Board
                 v-card-text This is a sample  
             v-flex
-              v-card(color="red" dark)
+              v-card
                 v-card-title Profile Bill Board
                 v-card-text This is a sample  
             v-flex
-              v-card(color="red" dark)
+              v-card
                 v-card-title Profile Bill Board
                 v-card-text This is a sample  
 
-        v-flex(d-flex xs12 sm6)
+        v-flex(d-flex).info-card
           v-layout(column)
             v-flex
-              v-card(color="orange" dark)
+              v-card
                 v-card-title Profile Bill Board
                 v-card-text This is a sample  
             v-flex
-              v-card(color="orange" dark)
+              v-card
                 v-card-title Profile Bill Board
                 v-card-text This is a sample  
             v-flex
-              v-card(color="orange" dark)
+              v-card
                 v-card-title Profile Bill Board
                 v-card-text This is a sample  
 
 </template>
 
 <style lang="stylus">
-.profile-pic
-  height 124px
-  width 124px
-  border-radius 50%
+_max-width = 156px + (48 * 2)px
+_card-max-width = 200px + (48 * 2)px
+_full-width = _max-width + 2 * (_card-max-width)
+_card-container-full-width = 2 * (_card-max-width)
+.at-width
+  max-width _max-width
+.profile-board
+  max-width _max-width 
+.info-card-container
+  max-width _card-container-full-width
+.info-card
+  max-width _card-max-width
+  min-width _card-max-width
+// xs breakpoint
+.profile-board-pad
+  padding (16 * 3)px
+
 @media screen and (max-width: 600px)
   .sm-row
     flex-direction row !important
+  .sm-col
+    flex-direction column !important
+  .sm-full
+    min-width 100% !important
+    max-width 100% !important
+  .info-card
+    @extend .sm-full 
+  .profile-board-pad
+    padding (16 * 1.5)px
+  .container
+    padding-left 0 !important
+    padding-right 0 !important
+
 @media screen and (min-width: 450px)
   .md-row
     flex-direction row
+
+@media screen and (max-width: _full-width)
+  .info-card-layout
+    flex-direction column
+  .info-card-container
+    max-width _card-max-width
+    min-width _card-max-width
+
 </style>
