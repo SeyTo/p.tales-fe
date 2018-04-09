6858af84b9d1472588df5e47e9002c2db7840478
rj <rajibbajracharya@gmail.com>
2018-04-09 22:08:14 +0545

    test utils for vue

--- test/unit/specs/Landing.spec.js
+++ test/unit/specs/Landing.spec.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import Landing from '@/views/Landing.vue'
-import NavBar from '@/shared/components/NavBar'
 
 // global.Vuetify = require('vuetify')
 Vue.use(Vuetify)
@@ -12,15 +11,6 @@ describe('Landing.vue', () => {
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('h1').textContent)
     .to.equal('Tales')
-  })
-})
-
-describe('Navbar.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(NavBar)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('h1').textContent)
-    .to.equal('Testing')
   })
 })
 
