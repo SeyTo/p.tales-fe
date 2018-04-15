735a67534e82c175177e6c8f21d3ee98c12c41d2
rj <rajibbajracharya@gmail.com>
2018-04-15 13:51:06 +0545

    updated layout for emp-search

--- src/views/emp-search.vue
+++ src/views/emp-search.vue
@@ -8,14 +8,21 @@ export default {
   name: 'emp-search',
 
   data () {
-    return { }
+    return {
+      filterHidden: false
+    }
   },
 
   components: {
     'tales-navbar': TalesNavBar,
     'tales-navdrawer': TalesNavDrawer,
     'emp-navbar-logged': EmpNavbarLogged
+  },
+
+  method: {
+    do () { }
   }
+
 }
 </script>
 
@@ -28,12 +35,54 @@ div
   tales-navdrawer
     emp-navbar-logged(compactView="true")
 
-  v-container
-    v-layout 
-      v-flex
-        | Under-construction
+  v-container(fluid)
+    .search-container
+      v-text-field(solo label="Search").search-bar
+
+    // -- main container
+    .body-container
+      // -- master filter (pre-filter)
+      v-card(ref="filter" v-if="!filterHidden")#filter.ml-3.mt-3
+        | master filter
+      .search-body
+        // -- post-filter for results
+        #result-filter(ref="resultFilter").ml-3.mt-3.mr-3
+          | result filter
+        // -- contains a list of results 
+        v-layout.result-container.ml-3.mt-3.mr-3
+          v-flex
+            | result container
+          
+  v-btn(@click.stop="filterHidden = !filterHidden") DO IT
 </template>
 
 
 <style lang="stylus">
+.search-container
+  width 100%
+  .search-bar
+    margin auto 
+    max-width 800px
+    background-color blue
+.body-container
+  display flex
+  background-color green
+  #filter
+    flex 0 0 auto
+    background-color aqua
+    min-height 500px
+    max-width 200px
+    min-width 200px
+  .search-body
+    flex 1 1 auto
+    background-color purple
+    #result-filter 
+      background-color yellow
+      min-height 40px
+
+    .result-container
+      background-color red
+      min-height 100px
+      min-width 100px
+    
 </style>
