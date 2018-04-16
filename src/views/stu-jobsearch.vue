2cdb42099dbc99c50c09ad548f8c6efc8631f7ff
rj <rajibbajracharya@gmail.com>
2018-04-16 19:26:25 +0545

    basic layout

--- src/views/stu-jobsearch.vue
+++ src/views/stu-jobsearch.vue
@@ -23,11 +23,31 @@ div
   stu-loggedin-navdraw
 
   v-container
-    v-layout
-      v-flex
-        | Under Construction
+    .search-container
+      v-text-field(solo label="Search").search-bar
+
+    // -- master filter
+    div#filter.ml-3.mt-3
+      | master filter
+
+    // -- result container
+    v-layout(wrap)#result-container
+      v-flex(v-for="i in 10")
+          v-card {{ i }} Testing
 </template>
 
 
 <style lang="stylus" scoped>
+.search-container
+  width 100%
+#filter
+  backgroud-color green
+  width 100%
+#result-container
+  backgroud-color red
+  width 100%
+  .card
+    max-width 304px
+    min-width 304px 
+    min-height 200px
 </style>
