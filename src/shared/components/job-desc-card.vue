ced4bc2074d3603ee06d66b04d823c14e694cd20
rj <rajibbajracharya@gmail.com>
2018-04-17 22:38:58 +0545

    new component & layout update

---
+++ src/shared/components/job-desc-card.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'job-desc-card',
+
+  props: [
+    'job'
+  ],
+
+  data () {
+    return { }
+  },
+
+  computed: { },
+
+  components: { }
+}
+</script>
+
+
+<template lang="pug">
+v-card
+  v-card-title {{ job.role }}
+  v-divider
+  v-container(fluid)
+    v-layout(column)
+      v-flex
+        | User Data
+      v-flex
+        | About Job
+      v-flex
+        | Deadline
+      v-flex
+        | Buttons bar
+
+
+</template>
+
+
+<style lang="stylus" scoped>
+</style>
--- src/views/stu/stu-jobs-search.vue
+++ src/views/stu/stu-jobs-search.vue
@@ -1,4 +1,5 @@
 <script>
+import JobDescCard from '../../shared/components/job-desc-card'
 
 /**
  * Jobs search page.
@@ -7,42 +8,70 @@ export default {
   name: 'stu-jobs-search',
 
   data () {
-    return { }
+    return {
+      job: {
+        role: 'Get me a tea gal'
+      }
+    }
   },
 
-  components: { }
+  components: {
+    'job-desc-card': JobDescCard
+  }
 }
 </script>
 
 
 <template lang="pug">
 v-container
-  // -- main search bar
-  .search-container
-    v-text-field(solo label="Search").search-bar
+  v-layout(column)
+    // -- main search bar
+    .search-container
+      v-text-field(solo label="Search").search-bar
 
-  // -- master filter
-  div#filter.ml-3.mt-3
-    | master filter
+    // -- master filter
+    div#filter.ml-3.mt-3
+      | master filter
 
-  // -- result container
-  v-layout(wrap)#result-container
-    v-flex(v-for="i in 10" :key="i")
-        v-card {{ i }} Testing
+    // -- result container
+    div.result-wrapper
+      div.result-container
+        div(v-for="i in 4" :key="i")
+          job-desc-card(:job="job") 
 </template>
 
 
 <style lang="stylus" scoped>
+_card_pad = (16 * 1)px
+_card_width = 250px
+_card_container_width_triple = (3 * _card_width) + (4 * _card_pad)
+_card_container_width_double = (2 * _card_width) + (3 * _card_pad)
+_card_container_width_single = (1 * _card_width) + (2 * _card_pad)
 .search-container
   width 100%
 #filter
-  backgroud-color green
+  background-color blue
   width 100%
-#result-container
-  backgroud-color red
-  width 100%
-  .card
-    max-width 304px
-    min-width 304px 
-    min-height 200px
+.result-wrapper
+  background-color green
+  display flex
+  justify-content center
+  .result-container
+    background-color red
+    width _card_container_width_triple
+    display flex
+    justify-content flex-start 
+    flex-wrap wrap
+    padding-right _card_pad
+    .card
+      max-width _card_width
+      width _card_width
+      min-height 200px
+      margin _card_pad 0 0 _card_pad 
+@media screen and (max-width: _card_container_width_triple)
+  .result-container
+    width _card_container_width_double !important
+@media screen and (max-width: _card_container_width_double)
+  .result-container
+    width _card_container_width_single !important
 </style>
