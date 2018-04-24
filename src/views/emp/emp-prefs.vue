f68c32ce208431ca5f4c5350f73a979a98bb918c
rj <rajibbajracharya@gmail.com>
2018-04-24 11:16:20 +0545

    layout

---
+++ src/views/emp/emp-prefs.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'emp-prefs',
+
+  data () {
+    return { }
+  },
+
+  computed: { },
+
+  components: { }
+}
+</script>
+
+
+<template lang="pug">
+div
+  h1 emp-prefs 
+</template>
+
+
+<style lang="stylus" scoped>
+</style>
--- src/views/stu/stu-prefs.vue
+++ src/views/stu/stu-prefs.vue
@@ -2,19 +2,108 @@
 export default {
   name: 'stu-prefs',
 
-  data () {
-    return { }
-  }
+  data: () => ({
+    valid: false,
+    jobPrefs: {
+      jobNotifDays: 'None',
+      jobNotifDaysItems: [ 'None', '2 days', '7 days', '10 days' ]
+    }
+  }),
+
+  methods: { }
 
 }
 </script>
 
 
 <template lang="pug">
+mixin jobprefs
+  v-flex(xs12).display-1 Job Preferences
+  v-flex(xs12).header
+    | Select jobs you want to hear most about
+  v-flex(xs12).job-pref-selector
+    v-layout
+      v-flex
+        v-flex
+          v-checkbox(hide-details :label="'Creative'")
+        v-flex
+          v-checkbox(hide-details :label="'Office & Operations'")
+        v-flex
+          v-checkbox(hide-details :label="'Outdoor & Physical'")
+        v-flex
+          v-checkbox(hide-details :label="'Sales & Marketing'")
+        v-flex
+          v-checkbox(hide-details :label="'Product Engineering'")
+      
+    
+  div.header
+    v-layout
+      v-subheader(v-text="'I want to receive personlized job notifications every '")
+      v-select(
+        dense 
+        v-model="jobPrefs.jobNotifDays" 
+        :items="jobPrefs.jobNotifDaysItems"
+        hide-details
+        solo
+      ).small-width
+
+mixin privacy
+  v-flex(xs12).display-1 Privacy
+  v-flex(column)
+    v-checkbox(hide-details :label="'Make my profile publicly searchable'")
+    v-checkbox(hide-details :label="'Let businesses find me'")
+
+mixin password
+  v-flex(xs12).display-1 Password
+  a Change Password
+
+mixin other
+  v-flex(xs12).display-1 Misc
+  v-flex(column)
+    v-checkbox(hide-details :label="'Receive Tales news and other updates.'")
+    v-checkbox(hide-details :label="'Receive Emails with weekly tips and other stuffs'")
+
+mixin accounts 
+  v-flex(xs12).display-1 Account Settings
+  a Suspend my account
+
 div
-  h1 Student Prefs
+  v-form(v-model="valid")
+    v-container
+      v-layout(row wrap)
+        +jobprefs
+      v-layout(row wrap)
+        +privacy
+      v-layout(row wrap)
+        +password
+      v-layout(row wrap)
+        +other
+      v-layout(row wrap)
+        +accounts
+      v-btn(xs12 block color="primary") Finish
+
 </template>
 
 
 <style lang="stylus" scoped>
+.container
+  width 50%
+.subheader 
+  height 64px
+.display-1
+  margin 16px 0px
+.small-width
+  width 150px
+.job-pref-selector
+  border 1px solid #000
+  border-radius 2px
+@media screen and (max-width: 800px)
+  .container
+    width 90% !important
+@media screen and (max-width: 600px)
+  .subheader
+    padding-top 16px
+    padding-left 0px
+    height 16px
+
 </style>
